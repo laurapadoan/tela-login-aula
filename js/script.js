21adbee621c8c2df83e2ff9c0fddecb086d6cbf8
laurapadoan40 <laura.padoan@aluno.senai.br>
2024-10-24 14:24:41 -0300

    tela.login

---
+++ js/script.js
@@ -0,0 +1,13 @@
+script.js >
+form.addEventListener(submit") callback
+ const form = document.getElementById ('form');
+  const email_ input = document.getElementById(' email-input');
+
+const password_input = document.getElementById ('password-input');
+ const error_message = document getElementById ('error-message');
+v form. addEventListener ('submit', (e) =>
+e. preventDefault);
+alert (email_input.value);
+error_message. innerText = password_input. value;
+5) ;
+Teacoc
